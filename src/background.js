e533ba53b5d5bdf0962be1afd6b1d1d5122c6b90
Otto Berger <otto@bergerdata.de>
2023-09-19 17:48:45 +0200

    convert to full-webextension

--- src/background.js
+++ src/background.js
@@ -1,18 +1,100 @@
 (async () => {
 
-    messenger.WindowListener.registerChromeUrl([
-        ["content", "quickarchiver", "chrome/content/"],
-        ["resource", "quickarchiver", "chrome/"],
-        ["locale", "quickarchiver", "en-US", "chrome/locale/en-US/"],
-        ["locale", "quickarchiver", "de-DE", "chrome/locale/de-DE/"]
-    ]);
+    messenger.messageDisplay.onMessageDisplayed.addListener(async (tab, message) => {
 
-    messenger.WindowListener.registerOptionsPage("chrome://quickarchiver/content/options.xhtml")
+        messenger.messageDisplayAction.setTitle({title: 'No role found'});
+        messenger.messageDisplayAction.setLabel({label: 'QuickArchiver'});
+        messenger.messageDisplayAction.disable();
 
-    messenger.WindowListener.registerWindow(
-        "chrome://messenger/content/messenger.xhtml",
-        "chrome://quickarchiver/content/quickarchiver_messenger.js");
+        let full = await messenger.messages.getFull(message.id);
+        let folder = await quickarchiver.getFolder(full.headers);
 
-    messenger.WindowListener.startListening();
+        if (folder) {
+
+            messenger.messageDisplayAction.enable();
+            messenger.messageDisplayAction.setTitle({title: 'Move "' + message.subject + '" to Folder ' + folder.path});
+            messenger.messageDisplayAction.setLabel({label: 'QuickArchiver: Move'});
+
+            console.info("Create menu");
+            await messenger.menus.create({
+                contexts: ["message_display_action"],
+                id: 'qa_edit',
+                title: "Edit QuickArchiver rule",
+                onclick: awaitPopup
+            });
+        }
+    });
+
+    messenger.commands.onCommand.addListener(async (command, tab) => {
+
+        if (command === "quickarchiver_move") {
+            let message = await messenger.messageDisplay.getDisplayedMessage(tab.id);
+            await quickarchiver.moveMail(message);
+        }
+    });
+
+    messenger.messageDisplayAction.onClicked.addListener(async (tab, info) => {
+
+        let message = await messenger.messageDisplay.getDisplayedMessage(tab.id);
+        await quickarchiver.moveMail(message);
+    });
+
+    //onMoved listener. fired when tab is moved into the same window
+    messenger.messages.onMoved.addListener(async (originalMessages, movedMessages) => {
+
+        await quickarchiver.movedMessages(movedMessages.messages);
+    });
+
+
+    // Function to open a popup and await user feedback
+    async function awaitPopup() {
+        async function popupPrompt(popupId, defaultResponse) {
+            try {
+                await messenger.windows.get(popupId);
+            } catch (e) {
+                // Window does not exist, assume closed.
+                return defaultResponse;
+            }
+            return new Promise(resolve => {
+                let response = defaultResponse;
+
+                function windowRemoveListener(closedId) {
+                    if (popupId == closedId) {
+                        messenger.windows.onRemoved.removeListener(windowRemoveListener);
+                        messenger.runtime.onMessage.removeListener(messageListener);
+                        resolve(response);
+                    }
+                }
+
+                function messageListener(request, sender, sendResponse) {
+                    if (sender.tab.windowId != popupId || !request) {
+                        return;
+                    }
+
+                    if (request.popupResponse) {
+                        response = request.popupResponse;
+                    }
+                }
+
+                messenger.runtime.onMessage.addListener(messageListener);
+                messenger.windows.onRemoved.addListener(windowRemoveListener);
+            });
+        }
+
+        let window = await messenger.windows.create({
+            url: "content/popup.html",
+            type: "popup",
+            height: 280,
+            width: 390,
+            allowScriptsToClose: true,
+        });
+        // Wait for the popup to be closed and define a default return value if the
+        // window is closed without clicking a button.
+        let rv = await popupPrompt(window.id, "cancel");
+        console.log(rv);
+    }
+
+// Listener to trigger the popup.
+    //messenger.browserAction.onClicked.addListener(awaitPopup);
 
 })()
---
+++ src/content/scripts/quickarchiver.js
@@ -0,0 +1,128 @@
+let quickarchiver = {
+
+    rules: {},
+
+    init: async function () {
+
+        return new Promise((resolve) => {
+            resolve(true);
+        });
+
+    },
+
+    movedMessages: async function (messages) {
+
+        for (let i in messages) {
+            let message = messages[i];
+
+            console.info("Moved message:");
+
+            await this.updateRule("from", this.parseEmail(message.author), message.folder)
+
+        }
+    },
+    initRules: async function () {
+        let rules = await messenger.storage.local.get('rules');
+
+        if (typeof (rules.rules) === "undefined") {
+            this.rules = {
+                from: []
+            };
+        } else {
+            this.rules = rules.rules;
+        }
+
+        return new Promise((resolve) => {
+            resolve(true);
+        });
+    },
+    saveRules: async function () {
+        await messenger.storage.local.set({
+            rules: this.rules
+        });
+
+        return new Promise((resolve) => {
+            resolve(true);
+        });
+    },
+    getFolder: async function(header){
+
+        await this.initRules();
+
+        for (let i in this.rules.from) {
+
+            if (this.rules.from[i].value === this.parseEmail(header.from[0])) {
+                return new Promise((resolve) => {
+                    resolve(this.rules.from[i].folder);
+                });
+            }
+        }
+
+    },
+    updateRule: async function (type, value, folder) {
+
+        if (typeof(folder.type) !== "undefined" && folder.type === "inbox"){
+
+            console.info("Ignore the Inbox folder!")
+
+            return new Promise((resolve) => {
+                resolve(true);
+            });
+        }
+
+        await this.initRules();
+
+        let found_index = false;
+
+        for (let i in this.rules[type]) {
+
+            if (this.rules[type][i].value === value) {
+                found_index = i;
+            }
+        }
+
+        if (found_index !== false) {
+            // update rule
+            this.rules[type][found_index].folder = folder;
+        } else {
+            // add rule
+            this.rules[type].push({
+                value: value,
+                folder: folder
+            });
+        }
+
+        await this.saveRules();
+
+        return new Promise((resolve) => {
+            resolve(true);
+        });
+    },
+    moveMail: async function(message) {
+
+        let full = await messenger.messages.getFull(message.id);
+        let folder = await this.getFolder(full.headers);
+
+        if (folder) {
+            try {
+                await messenger.messages.move([message.id], folder);
+                console.info("Moved message with id " + message.id + " to folder " + folder.path);
+            } catch(ex) {
+                console.error(ex);
+            }
+        }
+    },
+    parseEmail: function (string) {
+
+        let email = string.match(/(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/g);
+
+        if (email === null) {
+            return '';
+        }
+        if (email.length > 1) {
+            return email;
+        } else {
+            return email[0];
+        }
+    },
+}
