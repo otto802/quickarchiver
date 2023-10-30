9c3d0656edc23717adfabd7e9e767649a23af2a0
Otto Berger <otto@bergerdata.de>
2023-10-30 16:35:39 +0100

    htmlSimpleTable.js: error handling

--- src/content/tab/htmlSimpleTable.js
+++ src/content/tab/htmlSimpleTable.js
@@ -41,9 +41,14 @@ class htmlSimpleTable {
 
     build() {
 
-        this.renderTable();
-        this.renderTableHeader();
-        this.renderTableBody();
+        try{
+            this.renderTable();
+            this.renderTableHeader();
+            this.renderTableBody();
+
+        } catch (e) {
+            console.debug(e);
+        }
 
         return this.table;
 
@@ -132,76 +137,5 @@ class htmlSimpleTable {
                 cell.appendChild(value);
             }
         }
-
-        /*
-                foreach ($this->data as $rowKey => $row) {
-
-                    $css_class = "";
-
-                    if (is_array($this->render_config["table"]) && array_key_exists(
-                        'row_highlighter',
-                        $this->render_config["table"]
-                    )) {
-
-                        $class = $this->render_config["table"]['row_highlighter']($row);
-                        if ($class) {
-                            $css_class = $class;
-                        }
-                    }
-
-                    if ($rowKey === count($this->data)-1 && isset($this->render_config["table"]['last_row']) && isset($this->render_config["table"]['last_row']['class'])){
-                        $css_class .= " ". $this->render_config["table"]['last_row']['class'];
-                    }
-
-
-                    $output[] = sprintf(
-                        '<tr%s>',
-                        ($css_class ? ' class="' . $css_class . '"' : '')
-                    );
-
-                    foreach ($this->render_config["fields"] as $field) {
-
-                        if (array_key_exists('default_formatter', $field)) {
-                            $str = $field['default_formatter']($row[$field["field"]]);
-                        } else {
-                            $str = $row[$field["field"]];
-                        }
-
-                        if (array_key_exists('formatter_row', $field)) {
-                            $str = $field['formatter_row']($row);
-                        }
-                        if (array_key_exists('formatter_row_key', $field)) {
-                            $str = $field['formatter_row']($row, $rowKey);
-                        }
-
-                        if ($rowKey === count($this->data)-1 && isset($this->render_config["table"]['last_row']) && isset($this->render_config["table"]['last_row']['default_formatter'])){
-                            $str = $this->render_config["table"]['last_row']['default_formatter']($row[$field["field"]]);
-                        }
-
-                        if (array_key_exists('cell_highlighter', $field)) {
-                            $field["class"] .= " " . $field['cell_highlighter']($str, $row);
-                        }
-
-                        if ($field["cut"]) {
-                            $str = cutstr($str, $field["cut"]);
-                        }
-
-
-
-                        $output[] = sprintf(
-                            '<td%s%s>%s</td>',
-                            ($field["style"] ? ' style="' . $field["style"] . '"' : ''),
-                            ($field["class"] ? ' class="' . $field["class"] . '"' : ''),
-                            $str
-
-                        );
-                    }
-
-                    $output[] = '</tr>';
-                }
-                $output[] = '</tbody>';
-
-                return $output;*/
     }
-
 }
